0183c7bed3dde6bb7cbe10678d07a8414895ba2a
Mateo Robalino <alexrobalino27@gmail.com>
2022-05-16 19:49:24 -0500

    Creando Proyecto Prueba

--- app/src/main/java/poveda/uta/israelpoveda/Metodos.java
+++ app/src/main/java/poveda/uta/israelpoveda/Metodos.java
@@ -1,2 +1,22 @@
-package poveda.uta.israelpoveda;public class Metodos {
+package poveda.uta.israelpoveda;
+
+public class Metodos {
+    public int factorial(int num){
+        int i=1;
+        int f=1;
+        while(num>=i){
+            f*=i;
+            i++;
+        }
+        return f;
+    }
+    public int potencia(int num, int exp){
+        int i=1;
+        int p=1;
+        while(exp>=i){
+            p*=num;
+            i++;
+        }
+        return p;
+    }
 }
